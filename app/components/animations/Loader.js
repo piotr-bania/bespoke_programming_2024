ca75163325929790d64b28be374bef0f1328cfd2
Piotr <piotr.bania@europe.com>
2024-02-05 23:55:01 +0000

    test with animated light and shadows - gltfjsx feature!

--- app/components/animations/Loader.js
+++ app/components/animations/Loader.js
@@ -1,30 +1,17 @@
-import { useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
-import { useGLTF, Text } from '@react-three/drei'
+import { Text } from '@react-three/drei'
 
 const Loader = () => {
-    const logo = useGLTF('./models/logo.glb')
-    const loaderRef = useRef()
-
-    useFrame(() => {
-        loaderRef.current.rotation.y -= 0.01
-    })
-
     return (
-        <mesh position={[0, .125, 0]} >
-            <primitive ref={loaderRef} object={logo.scene} />
-            <Text
-                position={[0, -.25, 0]}
-                font='./fonts/neuropol.otf'
-                fontSize={.1}
-                color='white'
-                anchorX='center'
-                anchorY='middle'
-                textAlign='center'
-            >
-                Loading...
-            </Text>
-        </mesh>
+        <Text
+            font='./fonts/neuropol.otf'
+            fontSize={.1}
+            color='white'
+            anchorX='center'
+            anchorY='middle'
+            textAlign='center'
+        >
+            Loading...
+        </Text>
     )
 }
 
