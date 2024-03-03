f41328a2503075a6411aee458d4e12efd09dbcda
Piotr <piotr.bania@europe.com>
2024-03-03 13:49:04 +0000

    small update

--- app/about/page.js
+++ app/about/page.js
@@ -1,15 +1,24 @@
+import Button from '../components/ui/buttons/Button'
 import Heading_1 from '../components/ui/headings/Heading_1'
+import Paragraph from '../components/ui/paragraphs/Paragraph'
 import Route_Change from '../components/ui/transitions/Route_Change'
 
 const Room_1 = () => {
     return (
         <main>
             <Heading_1
-                h1Text='This is about page'
+                h1Text='Hi!'
                 className='heading_1'
             />
+            <Paragraph
+                pText='Welcome to the About Page'
+                className='paragraph'
+            />
             <Route_Change href='/'>
-                <button>Go to Home</button>
+                <Button
+                    buttonText='Go Home'
+                    className='button'
+                />
             </Route_Change>
         </main>
     )
